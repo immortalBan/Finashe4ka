960363f66a3b642231251363328ed8fe6b986923
Vane4ka <40294898+immortalBan@users.noreply.github.com>
2020-05-30 17:47:35 +0300

    шаше4ки
    тут все работает, но может сьезжать gui в зависимости от idle

---
+++ практика 5 защита/main.py
@@ -0,0 +1,14 @@
+import GameClass
+
+class MainClass:
+    """
+    Комментов не будет
+    Я пофиг, мне панк
+    """
+
+    def __init__(self):
+        self.session = GameClass.Game()
+
+
+if __name__ == '__main__':
+    MainClass()
