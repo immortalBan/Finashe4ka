05007a80fa86049e47df1c88b595a624b3215a25
Ivan <40294898+immortalBan@users.noreply.github.com>
2023-04-11 12:06:36 +0300

    fix

--- Web разработка/Семестр 2/Экзамен/app.js
+++ Web разработка/Семестр 2/Экзамен/app.js
@@ -3,8 +3,18 @@ const axios = require("axios");
 const app = express();
 const port = 5050;
 const server = require("http").createServer(app);
+const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
 const cors = require("cors");
+const db = new sqlite3.Database("cbr_daily.db");
+
+db.serialize(function () {
+  db.run("CREATE TABLE IF NOT EXISTS rates (rate TEXT, dt TEXT, change FLOAT)");
+});
+
+const insert_row = db.prepare(
+  "INSERT INTO rates (rate, dt, change) VALUES (?,?,?)"
+);
 
 app.use(
   express.urlencoded({
@@ -21,22 +31,56 @@ app.get("/", (req, res) => {
 });
 
 app.get("/get", (req, res) => {
+  var today = new Date();
+  var dd = String(today.getDate() + 1).padStart(2, "0");
+  var mm = String(today.getMonth() + 1).padStart(2, "0");
+  var yyyy = today.getFullYear();
+  today = yyyy + "-" + mm + "-" + dd;
   let response = null;
-  new Promise(async (resolve, reject) => {
-    try {
-      response = await axios("https://www.cbr-xml-daily.ru/daily_json.js");
-    } catch (error) {
-      response = null;
+  db.all(
+    `SELECT rate, dt, change FROM rates WHERE dt = ?`,
+    [today],
+    (err, results) => {
+      if (typeof results !== "undefined") {
+        console.log("from db");
+        let rates = {};
+        let date = results[0]["dt"];
+        for (i = 0; i < results.length; i++) {
+          rates[results[i]["rate"]] = results[i]["change"];
+        }
+        const json_response = {
+          date: date,
+          rates: rates,
+        };
+        res.send(json_response);
+      } else {
+        new Promise(async (resolve, reject) => {
+          try {
+            response = await axios("https://www.cbr-xml-daily.ru/latest.js");
+          } catch (error) {
+            response = null;
+          }
+          if (response) {
+            const json = response.data;
+            let date = json["date"];
+            let new_rates = {};
+            for (i = 0; i < Object.keys(json["rates"]).length; i++) {
+              let name = Object.keys(json["rates"])[i];
+              let value =
+                1 / parseFloat(json["rates"][Object.keys(json["rates"])[i]]);
+              insert_row.run(name, date, value);
+              new_rates[name] = value;
+            }
+            const json_response = {
+              date: date,
+              rates: new_rates,
+            };
+            res.send(json_response);
+          }
+        });
+      }
     }
-    if (response) {
-      const json = response.data;
-      const json_response = {
-        date: json["Date"],
-        value: json["Valute"],
-      };
-      res.send(json_response);
-    }
-  });
+  );
 });
 
 server.listen(port, function () {
--- Web разработка/Семестр 2/Экзамен/public/js/script.js
+++ Web разработка/Семестр 2/Экзамен/public/js/script.js
@@ -1,19 +1,25 @@
+let rates = [];
+
 async function getRate() {
   let url = window.location.href + "get";
 
   let response = await fetch(url);
   let data = await response.json();
-  let course = data["value"];
-  let output = `<tr><th>Валюта</th><th>Курс сегодня</th><th>Курс вчера</th></tr>`;
-  for (let i = 0; i < Object.keys(course).length; i++) {
-    let name = Object.keys(course)[i];
-    output += `<tr><td>${
-      course[name]["Nominal"] + " " + course[name]["Name"]
-    }</td><td>${course[name]["Value"]}</td><td>${
-      course[name]["Previous"]
-    }</td></tr>`;
+  rates = data["rates"];
+  let output = `<p>Валюта  <select id="rates_select" onchange=changeExchangeRate(this)><option disabled selected>Валюта</option>`;
+  for (let i = 0; i < Object.keys(rates).length; i++) {
+    let name = Object.keys(rates)[i];
+    output += `<option value="${name}">${name}</option>`;
   }
+  output += `</select></p>`;
   document.getElementById("main-table").innerHTML = output;
+}
+
+function changeExchangeRate(select) {
+  let rate_name = select.options[select.selectedIndex].value;
+  let rate_value = rates[rate_name];
+  document.getElementById("exchange_rate").value = rate_value;
+  console.log(rate_name, rate_value);
 }
 
 window.onload = async () => {
